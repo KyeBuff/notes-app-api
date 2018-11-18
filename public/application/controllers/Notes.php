a7e6054755a73ed6b9c0665ca73ac2aee48673af
Kye Buffery <kyebuffery14@gmail.com>
2018-11-18 11:00:14 +0000

    fixed red bean

--- public/application/controllers/Notes.php
+++ public/application/controllers/Notes.php
@@ -2,19 +2,55 @@
 
 require (APPPATH.'/libraries/REST_Controller.php'); 
 require (APPPATH.'/libraries/Format.php');
-require (APPPATH.'/libraries/rb-mysql.php');
 
 use Restserver\Libraries\REST_Controller;
 
 class Notes extends REST_Controller {
     
     public function __construct(){
-        parent::__construct();
+		parent::__construct();
+		$this->load->library('rb');
     }
 	/**
 	 * Index all notes
 	 */
 	public function index_get()
+	{
+        // Returns a list of notes
+        // $this->response($this->db->get('notes')->result());
+    }
+
+    /**
+	 * Index all notes
+	 */
+	public function index_id_get()
+	{
+        // Returns a list of notes
+        // $this->response($this->db->get('notes')->result());
+    }
+    
+    /**
+	 * Index all notes
+	 */
+	public function index_post()
+	{
+        // Returns a list of notes
+        // $this->response($this->db->get('notes')->result());
+    }
+    
+    /**
+	 * Index all notes
+	 */
+	public function index_put()
+	{
+        // Returns a list of notes
+        // $this->response($this->db->get('notes')->result());
+    }
+    
+    /**
+	 * Index all notes
+	 */
+	public function index_delete()
 	{
         // Returns a list of notes
         // $this->response($this->db->get('notes')->result());
